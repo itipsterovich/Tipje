ab4969460c89a2a6cd7733c48544dfd7cd7cab70
Ira Bykova <itipsterovich@gmail.com>
2025-05-21 10:42:13 +0200

    Initial Commit

---
+++ Tipje/TipjeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipjeApp.swift
+//  Tipje
+//
+//  Created by Ira Bykova on 21/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct TipjeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
