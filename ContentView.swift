7d55e1802a76cfd6bd376d937cff5134ffe60390
Ira Bykova <itipsterovich@gmail.com>
2025-05-30 12:21:02 +0200

    Add initial project files including ContentView, Info.plist, and various image assets with multiple resolutions and configurations.

---
+++ ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Tipje
+//
+//  Created by Ira Bykova on 30/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView()
+}
